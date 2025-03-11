03767908a36aaf4f7358aee405f3d56f2c76c20a
InstaGiftCharms <InstaGiftCharms@gmail.com>
2025-03-11 15:37:03 +0800

    Update configParameters.js

--- configParameters.js
+++ configParameters.js
@@ -31,50 +31,16 @@ class ConfigParameters {
         {
             productName: "Solid Letter Name KeyChains (150.00 PHP)",
             productForm: [ // Define form for Charm 1
-                { type: 'label', id: 'lbl_01', value: 'Keychain Name (Maximum of 4 letters):' },
-                { type: 'textbox', id: 'txt_charmMsg', value: 'Type here' },
+                { type: 'label', id: 'lbl_01', value: 'Keychain Name (Maximum of 4 letters):'},
+                { type: 'textbox', id: 'txt_charmMsg', value: 'Type here', ResponsePrefix:'Keychain name of ' },
                 { type: 'label', id: 'lbl_02', value: '[Need more letters? Select the "Extra Letter" add-on below to make it up to 5 letters.]' },
                 { type: 'label', id: 'lbl_03', value: 'Add-ons(Optional):' },
-                { type: 'img', id: 'img_charmAddon01', value: '"Extra Letter (+ 10.00 PHP)":"https://lh3.googleusercontent.com/pw/AP1GczNCGWT6EZ5yV-SeNfZ0-YbV96-4AqQAug30GouSFn9l9R17E0xbmlmof9Dg64aQrv2BC0hYenO95GrCmFEBDaNaoptc0gnz6NSoIOtFrhS6kOtsk24mLH3k-EMAZiIiaSpC8mV40V-pyWJKnKsyWLS4hQ=w600-h600-s-no-gm?authuser=0"' },
-                { type: 'img', id: 'img_charmAddon02', value: '"Big Charms (+ 10.00 PHP)":"https://lh3.googleusercontent.com/pw/AP1GczNCGWT6EZ5yV-SeNfZ0-YbV96-4AqQAug30GouSFn9l9R17E0xbmlmof9Dg64aQrv2BC0hYenO95GrCmFEBDaNaoptc0gnz6NSoIOtFrhS6kOtsk24mLH3k-EMAZiIiaSpC8mV40V-pyWJKnKsyWLS4hQ=w600-h600-s-no-gm?authuser=0"' },
-                { type: 'img', id: 'img_charmAddon03', value: '"Small Charms (+ 5.00 PHP)":"https://lh3.googleusercontent.com/pw/AP1GczNCGWT6EZ5yV-SeNfZ0-YbV96-4AqQAug30GouSFn9l9R17E0xbmlmof9Dg64aQrv2BC0hYenO95GrCmFEBDaNaoptc0gnz6NSoIOtFrhS6kOtsk24mLH3k-EMAZiIiaSpC8mV40V-pyWJKnKsyWLS4hQ=w600-h600-s-no-gm?authuser=0"' },
-                { type: 'img', id: 'img_charmAddon04', value: '"Phone Hook (+ 25.00 PHP)":"https://lh3.googleusercontent.com/pw/AP1GczNCGWT6EZ5yV-SeNfZ0-YbV96-4AqQAug30GouSFn9l9R17E0xbmlmof9Dg64aQrv2BC0hYenO95GrCmFEBDaNaoptc0gnz6NSoIOtFrhS6kOtsk24mLH3k-EMAZiIiaSpC8mV40V-pyWJKnKsyWLS4hQ=w600-h600-s-no-gm?authuser=0"' },
-                { type: 'quantity', id: 'num_itemQuantity', value: 'Item Quantity' }
+                { type: 'img', id: 'img_charmAddon01', value: '"Extra Letter (+ 10.00 PHP)":"https://lh3.googleusercontent.com/pw/AP1GczNCGWT6EZ5yV-SeNfZ0-YbV96-4AqQAug30GouSFn9l9R17E0xbmlmof9Dg64aQrv2BC0hYenO95GrCmFEBDaNaoptc0gnz6NSoIOtFrhS6kOtsk24mLH3k-EMAZiIiaSpC8mV40V-pyWJKnKsyWLS4hQ=w600-h600-s-no-gm?authuser=0"', ResponsePrefix:'Include ' },
+                { type: 'img', id: 'img_charmAddon02', value: '"Big Charms (+ 10.00 PHP)":"https://lh3.googleusercontent.com/pw/AP1GczNCGWT6EZ5yV-SeNfZ0-YbV96-4AqQAug30GouSFn9l9R17E0xbmlmof9Dg64aQrv2BC0hYenO95GrCmFEBDaNaoptc0gnz6NSoIOtFrhS6kOtsk24mLH3k-EMAZiIiaSpC8mV40V-pyWJKnKsyWLS4hQ=w600-h600-s-no-gm?authuser=0"', ResponsePrefix:'Include ' },
+                { type: 'img', id: 'img_charmAddon03', value: '"Small Charms (+ 5.00 PHP)":"https://lh3.googleusercontent.com/pw/AP1GczNCGWT6EZ5yV-SeNfZ0-YbV96-4AqQAug30GouSFn9l9R17E0xbmlmof9Dg64aQrv2BC0hYenO95GrCmFEBDaNaoptc0gnz6NSoIOtFrhS6kOtsk24mLH3k-EMAZiIiaSpC8mV40V-pyWJKnKsyWLS4hQ=w600-h600-s-no-gm?authuser=0"', ResponsePrefix:'Include ' },
+                { type: 'img', id: 'img_charmAddon04', value: '"Phone Hook (+ 25.00 PHP)":"https://lh3.googleusercontent.com/pw/AP1GczNCGWT6EZ5yV-SeNfZ0-YbV96-4AqQAug30GouSFn9l9R17E0xbmlmof9Dg64aQrv2BC0hYenO95GrCmFEBDaNaoptc0gnz6NSoIOtFrhS6kOtsk24mLH3k-EMAZiIiaSpC8mV40V-pyWJKnKsyWLS4hQ=w600-h600-s-no-gm?authuser=0"', ResponsePrefix:'Include ' },
+                { type: 'quantity', id: 'num_itemQuantity', value: 'Item Quantity', , ResponsePrefix:'Quantity: ' }
             ]
         },
-        {
-            productName: "Charm 2",
-            productForm: [ // Define form for Charm 2
-                { type: 'label', id: 'charm2Label', value: 'Charm 2 Options:' },
-                { type: 'radio', id: 'charm2Color', value: 'Red;Blue;Green' },
-                { type: 'quantity', id: 'charm2Quantity', value: 'Quantity' },
-                { type: 'img', id: 'charm2Image', value: '"Charm 2 Preview":"' + ConfigParameters.slideshowImages[1] + '"' } // Image for Charm 2 - URL from slideshow, WITH caption
-            ]
-        },
-        {
-            productName: "Charm 3",
-            productForm: [ // Define form for Charm 3
-                { type: 'label', id: 'charm3Label', value: 'Charm 3 Choices:' },
-                { type: 'checkbox', id: 'charm3Features', value: 'Feature A;Feature B;Feature C' },
-                { type: 'quantity', id: 'charm3Quantity', value: 'Quantity' },
-                { type: 'img', id: 'charm3Image', value: '"":"' + ConfigParameters.slideshowImages[2] + '"' } // Image for Charm 3 - URL from slideshow, NO caption
-            ]
-        },
-        {
-            productName: "Charm Set A",
-            productForm: [ // Define form for Charm Set A
-                { type: 'label', id: 'charmSetALabel', value: 'Charm Set A Customization:' },
-                { type: 'textbox', id: 'charmSetAText1', value: 'Enter text for part 1' },
-                { type: 'textbox', id: 'charmSetAText2', value: 'Enter text for part 2' },
-                { type: 'quantity', id: 'charmSetAQuantity', value: 'Quantity' }
-            ]
-        },
-        {
-            productName: "Custom Charm Design",
-            productForm: [ // Define form for Custom Charm Design
-                { type: 'label', id: 'customCharmLabel', value: 'Custom Charm Design Request:' },
-                { type: 'textarea', id: 'customCharmDescription', value: 'Describe your custom charm design' } // Textarea type is missing in DynamicForm, you might want to add it later. For now, using textbox as placeholder
-            ]
-        }
     ];
 }
